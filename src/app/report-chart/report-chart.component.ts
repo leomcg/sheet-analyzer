2efc6cce476374b208b53e3220791cb4ceacd2ed
leomcg <leomcg@gmail.com>
2023-02-24 22:38:51 -0300

    improving chart

--- src/app/report-chart/report-chart.component.ts
+++ src/app/report-chart/report-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 
 @Component({
@@ -6,31 +6,62 @@ import { Chart } from 'angular-highcharts';
   templateUrl: './report-chart.component.html',
   styleUrls: ['./report-chart.component.scss']
 })
-export class ReportChartComponent {
-  chartData = new Chart({
-    title: {
-      text: 'OI'
-    },
-    yAxis :{
-      min:0,
-      max:100
-    },
-    series: [
-      {
-        name: 'POUCA COMPLEXIDADE',
-        type: 'column',
-        data: [30]
+export class ReportChartComponent implements OnInit {
+
+  totalSheets = 300
+  self = this
+  chartData = new Chart()
+
+  ngOnInit(): void {
+    console.log('onInit()')
+    const self = this
+    this.chartData = new Chart({
+      title: {
+        text: 'Gráfico de Pareto'
       },
-      {
-        name: 'MÉDIA COMPLEXIDADE',
-        type: 'column',
-        data: [40]
+      yAxis :{
+        min:0,
+        max:100,
+        labels: {
+          formatter: function (data) {
+              return data.value + ' %'
+          }
+        },
+        title: {
+          text: "Porcentagens"
+        }
       },
-      {
-        name: 'POUCA COMPLEXIDADE',
-        type: 'column',
-        data: [30]
-      }
-    ]
-  })
+      tooltip: {
+        formatter: function(data) {
+          console.log('data: ', data)
+          console.log('this.y: ', this.y)
+          console.log('self.totalSheets: ', self.totalSheets)
+          if(this.y) {
+            return `Quantidade: ${this.y} | Porcentagem: ${(this.y / self.totalSheets)  * 100}% `
+          }
+          return ''
+        }
+      },
+      series: [
+        {
+          name: 'POUCA COMPLEXIDADE',
+          type: 'column',
+          data: [20]
+        },
+        {
+          name: 'MÉDIA COMPLEXIDADE',
+          type: 'column',
+          data: [50]
+        },
+        {
+          name: 'POUCA COMPLEXIDADE',
+          type: 'column',
+          data: [30]
+        }
+      ]
+    })
+  }
 }
+ 
+
+
